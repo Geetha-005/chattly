e0308a15857ca2153a5e07f06f71280cf0c3037b
Geetha-005 <chillageetha323@gmail.com>
2025-07-04 00:01:19 +0530

    auth ui and api integration finished

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import cookieParser from "cookie-parser" 
 import connectDB from './config/db.js'
 import authRouter from './routes/auth.routes.js'
-
+import cors from 'cors'
 dotenv.config()
 
 
@@ -11,6 +11,10 @@ const port=process.env.PORT||5000
 
 const app=express()
 
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRouter)
