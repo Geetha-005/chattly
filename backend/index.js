360415bb53ff30ff84dd22e25144075e500ad725
love coding <chillageetha323@gmail.com>
2025-07-06 00:52:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://chattly-eav1.onrender.com",
   credentials: true
 }));
 app.use(express.json());
